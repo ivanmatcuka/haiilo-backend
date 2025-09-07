411c151ce11ced99f066d8ea6a9daabff2553b20
Ivan Matcuka <ivanmatcuka@protonmail.com>
2025-09-07 18:56:24 +0200

    chore: initial

---
+++ models/offer.ts
@@ -0,0 +1,10 @@
+export type Offer = {
+  id: string;
+  itemId: string;
+  numberOfItems: number;
+  price: number;
+  validFrom: Date;
+  validTo: Date;
+  createdAt: Date;
+  updatedAt: Date;
+};
