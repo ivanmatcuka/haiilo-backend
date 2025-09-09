597c2208ac959980f09b08ae65470234250a9788
Ivan Matcuka <ivanmatcuka@protonmail.com>
2025-09-09 13:39:48 +0200

    refactor: views removed

--- routes/index.ts
+++ routes/index.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 router.get("/", (_: Request, res: Response) => {
-  res.render("index", { title: "Express" });
+  res.send("API is running...");
 });
 
 export default router;
